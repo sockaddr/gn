4638bff3fa4c0ee67c0424d6235b6acea6dba731
sockaddr <90645986+sockaddr@users.noreply.github.com>
2022-08-29 16:38:46 +0200

    Thread now reports its state

--- src/wrkr/gn_conn_mgmt_thrd.c
+++ src/wrkr/gn_conn_mgmt_thrd.c
@@ -7,9 +7,19 @@ gn_conn_mgmt_thrd (void * const p)
 {
   if (p == NULL) return NULL; // TODO: Log error.
 
+  gn_conn_mgmt_thrd_conf_s * const conn_mgmt_thrd_conf = p;
+  conn_mgmt_thrd_conf->state = CONN_MGMT_THRD_RUNNING;
+
   while (true) {
     sleep (1); // TODO: Remove.
+    if (conn_mgmt_thrd_conf->stop) {
+      if (conn_mgmt_thrd_conf->state != CONN_MGMT_THRD_STOPPING) {
+        conn_mgmt_thrd_conf->state = CONN_MGMT_THRD_STOPPING;
+      }
+    }
   }
+
+  conn_mgmt_thrd_conf->state = CONN_MGMT_THRD_STOPPED;
 
   return NULL; // TODO: Maybe return something else or store exit code in conf struct.
 }
--- src/wrkr/hdr/gn_conn_mgmt_thrd.h
+++ src/wrkr/hdr/gn_conn_mgmt_thrd.h
@@ -1,7 +1,9 @@
 #ifndef GN_CONN_MGMT_THRD_H
 #define GN_CONN_MGMT_THRD_H
 
-#include <stdbool.h>
-#include <stddef.h>
+#include <wrkr/hdr/gn_conn_mgmt_thrd_conf_s.h>
+
+// #include <stdbool.h>
+//#include <stddef.h>
 
 #endif // GN_CONN_MGMT_THRD_H
