9227cf6c99d30824cbe1ae68a35f0a329cdf5d82
sockaddr <90645986+sockaddr@users.noreply.github.com>
2022-08-29 16:35:04 +0200

    Added state member/enum for thread state info

--- src/wrkr/hdr/gn_conn_mgmt_thrd_conf_s.h
+++ src/wrkr/hdr/gn_conn_mgmt_thrd_conf_s.h
@@ -1,14 +1,19 @@
 #ifndef GN_CONN_MGMT_THRD_CONF_S_H
 #define GN_CONN_MGMT_THRD_CONF_S_H
 
+#include <wrkr/hdr/gn_conn_mgmt_thrd_state_e.h>
+
 #include <pthread.h>
+#include <stdbool.h>
 
 typedef struct gn_conn_mgmt_thrd_conf_s gn_conn_mgmt_thrd_conf_s;
 
 struct gn_conn_mgmt_thrd_conf_s {
-  pthread_t thrd_id;
+  bool                       stop;
+  gn_conn_mgmt_thrd_state_e  state;
   gn_conn_mgmt_thrd_conf_s * prev;
   gn_conn_mgmt_thrd_conf_s * next;
+  pthread_t                  thrd_id;
 };
 
 #endif // GN_CONN_MGMT_THRD_CONF_S_H
---
+++ src/wrkr/hdr/gn_conn_mgmt_thrd_state_e.h
@@ -0,0 +1,12 @@
+#ifndef GN_CONN_MGMT_THRD_STATE_E_H
+#define GN_CONN_MGMT_THRD_STATE_E_H
+
+enum gn_conn_mgmt_thrd_state_e {
+  CONN_MGMT_THRD_RUNNING,
+  CONN_MGMT_THRD_STOPPING,
+  CONN_MGMT_THRD_STOPPED
+};
+
+typedef enum gn_conn_mgmt_thrd_state_e gn_conn_mgmt_thrd_state_e;
+
+#endif // GN_CONN_MGMT_THRD_STATE_E_H
