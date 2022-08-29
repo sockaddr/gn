96f6d968bcac3c32571c90f0facfd22d0a40a1ea
sockaddr <90645986+sockaddr@users.noreply.github.com>
2022-08-29 15:54:00 +0200

    Added func to push a thread conf struct to list

---
+++ src/wrkr/gn_conn_mgmt_thrd_conf_list_push_back.c
@@ -0,0 +1,31 @@
+#include <wrkr/hdr/gn_conn_mgmt_thrd_conf_list_push_back.h>
+
+/*
+ * TODO: Add description.
+ */
+
+bool
+gn_conn_mgmt_thrd_conf_list_push_back (gn_conn_mgmt_thrd_conf_list_s * const list,
+                                       gn_conn_mgmt_thrd_conf_s * const      conf)
+{
+  // list->len is a uint16_t
+  switch (list->len) {
+    case UINT16_MAX: {
+      return true;
+    }
+    case 0: {
+      list->head = list->tail = conf->prev = conf->next = conf;
+      break;
+    }
+    default: {
+      list->tail->next = conf;
+      conf->prev = list->tail;
+      list->head->prev = conf;
+      conf->next = list->head;
+      list->tail = conf;
+    }
+  }
+
+  list->len++;
+  return false;
+}
---
+++ src/wrkr/hdr/gn_conn_mgmt_thrd_conf_list_push_back.h
@@ -0,0 +1,8 @@
+#ifndef GN_CONN_MGMT_THRD_CONF_LIST_PUSH_BACK_H
+#define GN_CONN_MGMT_THRD_CONF_LIST_PUSH_BACK_H
+
+#include <wrkr/hdr/gn_conn_mgmt_thrd_conf_list_s.h>
+
+#include <stdbool.h>
+
+#endif // GN_CONN_MGMT_THRD_CONF_LIST_PUSH_BACK_H
