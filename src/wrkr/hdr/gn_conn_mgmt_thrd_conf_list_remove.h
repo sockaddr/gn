26acbd614a066cce5e03009a0725eb1b99e2ed50
sockaddr <90645986+sockaddr@users.noreply.github.com>
2022-08-29 18:39:22 +0200

    Added func to remove connection mgmt conf list element

---
+++ src/wrkr/hdr/gn_conn_mgmt_thrd_conf_list_remove.h
@@ -0,0 +1,6 @@
+#ifndef GN_CONN_MGMT_THRD_CONF_LIST_REMOVE_H
+#define GN_CONN_MGMT_THRD_CONF_LIST_REMOVE_H
+
+#include <wrkr/hdr/gn_conn_mgmt_thrd_conf_list_s.h>
+
+#endif // GN_CONN_MGMT_THRD_CONF_LIST_REMOVE_H
---
+++ src/wrkr/hdr/gn_stop_conn_mgmt_thrds.h
@@ -0,0 +1,11 @@
+#ifndef GN_STOP_CONN_MGMT_THRDS_H
+#define GN_STOP_CONN_MGMT_THRDS_H
+
+#include <wrkr/hdr/gn_conn_mgmt_thrd_conf_list_s.h>
+
+// TODO: Maybe add cold attribute.
+void
+gn_conn_mgmt_thrd_conf_list_remove (gn_conn_mgmt_thrd_conf_list_s * const,
+                                    const gn_conn_mgmt_thrd_conf_s * const);
+
+#endif // GN_STOP_CONN_MGMT_THRDS_H
