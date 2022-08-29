c51a9386fa669586938196b5e5eba8212ba72803
sockaddr <90645986+sockaddr@users.noreply.github.com>
2022-08-29 18:39:42 +0200

    Minor changes

--- compile.sh
+++ compile.sh
@@ -9,7 +9,9 @@ src/main.c \
 src/wrkr/gn_conn_mgmt_thrd.c \
 src/wrkr/gn_conn_mgmt_thrd_conf_init.c \
 src/wrkr/gn_conn_mgmt_thrd_conf_list_push_back.c \
+src/wrkr/gn_conn_mgmt_thrd_conf_list_remove.c \
 src/wrkr/gn_start_conn_mgmt_thrds.c \
+src/wrkr/gn_stop_conn_mgmt_thrds.c \
 src/wrkr/gn_wrkr_main.c \
 \
 -lpthread
--- src/wrkr/gn_conn_mgmt_thrd.c
+++ src/wrkr/gn_conn_mgmt_thrd.c
@@ -1,5 +1,6 @@
 #include <wrkr/hdr/gn_conn_mgmt_thrd.h>
 
+#include <stdio.h> // TODO: Remove.
 #include <unistd.h> // TODO: Remove.
 
 void *
@@ -15,10 +16,12 @@ gn_conn_mgmt_thrd (void * const p)
     if (conn_mgmt_thrd_conf->stop) {
       if (conn_mgmt_thrd_conf->state != CONN_MGMT_THRD_STOPPING) {
         conn_mgmt_thrd_conf->state = CONN_MGMT_THRD_STOPPING;
+        break;
       }
     }
   }
 
+  printf ("CMT %p stopped\n", conn_mgmt_thrd_conf);
   conn_mgmt_thrd_conf->state = CONN_MGMT_THRD_STOPPED;
 
   return NULL; // TODO: Maybe return something else or store exit code in conf struct.
