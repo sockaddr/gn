88c8f462cf6b1daa72b24171152f10ff8a5138bc
sockaddr <90645986+sockaddr@users.noreply.github.com>
2022-08-29 15:58:00 +0200

    Minor changes and added ptrs for doubly-linked list

--- src/wrkr/hdr/gn_conn_mgmt_thrd_conf_s.h
+++ src/wrkr/hdr/gn_conn_mgmt_thrd_conf_s.h
@@ -3,10 +3,12 @@
 
 #include <pthread.h>
 
+typedef struct gn_conn_mgmt_thrd_conf_s gn_conn_mgmt_thrd_conf_s;
+
 struct gn_conn_mgmt_thrd_conf_s {
   pthread_t thrd_id;
+  gn_conn_mgmt_thrd_conf_s * prev;
+  gn_conn_mgmt_thrd_conf_s * next;
 };
-
-typedef struct gn_conn_mgmt_thrd_conf_s gn_conn_mgmt_thrd_conf_s;
 
 #endif // GN_CONN_MGMT_THRD_CONF_S_H
--- src/wrkr/hdr/gn_wrkr_main.h
+++ src/wrkr/hdr/gn_wrkr_main.h
@@ -3,6 +3,7 @@
 
 #include <wrkr/hdr/gn_conn_mgmt_thrd_conf_list_s.h>
 
-void gn_start_conn_mgmt_thrds (gn_conn_mgmt_thrd_conf_list_s * const);
+void gn_start_conn_mgmt_thrds (gn_conn_mgmt_thrd_conf_list_s * const)
+   __attribute__((cold));
 
 #endif // GN_WRKR_MAIN_H
