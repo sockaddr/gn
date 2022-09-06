441a6a53a98d016164de68f8450782b07212c32b
sockaddr <90645986+sockaddr@users.noreply.github.com>
2022-09-06 20:05:22 +0200

    Allow starting without connection acpt threads

--- src/wrkr/gn_wrkr_conf_init.c
+++ src/wrkr/gn_wrkr_conf_init.c
@@ -13,6 +13,7 @@ gn_wrkr_conf_init (gn_wrkr_conf_s * const conf)
   conf->conn_acpt_thrd_num = 1;
   (void)! gn_conn_mgmt_thrd_conf_list_init (&conf->conn_mgmt_thrd_conf_list);
   conf->conn_mgmt_thrd_num = 1;
+  conf->start_wout_conn_acpt_thrds = true;
   conf->start_wout_conn_mgmt_thrds = true;
 
   return false;
