7656140daf6e38cffe5aa00f5dbb5acdd3cccac8
sockaddr <90645986+sockaddr@users.noreply.github.com>
2022-09-18 11:26:05 +0200

    Created socketpair for master/worker IO

--- src/mstr/gn_start_wrkr.c
+++ src/mstr/gn_start_wrkr.c
@@ -1,19 +1,37 @@
 #include <mstr/hdr/gn_start_wrkr.h>
 
+/*
+ * TODO: Add description.
+ */
+
 void
 gn_start_wrkr (char * const self_path, gn_lstnr_conf_list_s * const lstnr_conf_list)
 {
+  // Create socket pair to send configuration and sockets to worker process.
+  int sp[2] = {-1, -1}; // TODO: Store them in worker proc entry.
+  const int rsocketpair = socketpair (AF_UNIX, SOCK_CLOEXEC | SOCK_NONBLOCK | SOCK_STREAM, 0, sp);
+  if (rsocketpair != 0) {
+    error_at_line (0, errno, __FILE__, __LINE__, "socketpair() failed");
+    return;
+  }
+
   const pid_t rfork = fork ();
   switch (rfork) {
     case 0: { // Child
       char * const argv[3] = {self_path, "--worker", NULL};
       execv (self_path, argv);
-      break;
+      error_at_line (0, errno, __FILE__, __LINE__, "execv() failed");
+      exit (1);
     }
     case -1: {
+      error_at_line (0, errno, __FILE__, __LINE__, "fork() failed");
       break;
     }
     default: { // Parent
+      return;
     }
   }
+
+  close (sp[0]);
+  close (sp[1]);
 }
--- src/mstr/hdr/gn_start_wrkr.h
+++ src/mstr/hdr/gn_start_wrkr.h
@@ -3,7 +3,11 @@
 
 #include <comn/hdr/gn_lstnr_conf_list_s.h>
 
+#include <errno.h>
+#include <error.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/socket.h>
 
 #endif // GN_START_WRKR_H
